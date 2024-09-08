dc2e5f00b490e4f19a6786a5d102eeb187d53a17
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-09-08 20:40:11 +0900

    FEAT : HTTP Method practice

---
+++ src/main/java/hello/servlet/basic/requestmapping/MappingController.java
@@ -0,0 +1,47 @@
+package hello.servlet.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    /**
+     *
+     기본 요청
+     *
+     둘다 허용 /hello-basic, /hello-basic/
+     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+
+    @GetMapping(value = "/hello-basic")
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable String userId) {
+        log.info("mappingPath userId={}", userId);
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId){
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+
+
+}
